b5729a623e92d14da5794a6e7571de99ffc24f9b
soufcod3 <soufiane.ait@outlook.fr>
2023-12-25 14:39:08 +0100

    responsive : medium

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,16 +3,16 @@ import { Countdown } from "./Countdown"
 
 export const Hero = () => {
   return (
-    <section className="mt-10 flex flex-nowrap justify-center">
-      <div className="main-text basis-1/2 ps-20 flex flex-col items-end pe-20">
-        <h1 className="text-7xl font-black" style={{ width: "30rem" }}>
+    <section className="mt-10 flex flex-col xl:flex-row flex-nowrap justify-center gap-4 xl:gap-0">
+      <div className="basis-1/2 ps-20 flex flex-col items-center xl:items-end pe-20 bg-red-500">
+        <h1 className="text-7xl font-black text-center xl:text-start" style={{ width: "30rem" }}>
           Transformez votre idée en réalité avec le no-code
         </h1>
-        <p className="text-xl pt-3 text-purple-100" style={{ width: "30rem" }}>
+        <p className="text-xl pt-3 text-purple-100 text-center xl:text-start" style={{ width: "30rem" }}>
           Créez votre site web, même si vous n'avez jamais codé auparavant.
         </p>
       </div>
-      <div className="main-animation basis-1/2">
+      <div className="basis-1/2 flex justify-center xl:justify-start bg-green-500">
         <div style={{ width: "35rem" }}>
           <CanvasAnimation/>
           <div className="flex mt-3">
