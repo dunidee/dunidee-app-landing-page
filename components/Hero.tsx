69e4404c61791482dcf07fca113fb36d9d23f6d7
soufcod3 <soufiane.ait@outlook.fr>
2023-12-25 15:22:43 +0100

    fix: mobile responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,36 +1,36 @@
-import { CanvasAnimation } from "./CanvasAnimation"
-import { Countdown } from "./Countdown"
+import { CanvasAnimation } from "./CanvasAnimation";
+import { Countdown } from "./Countdown";
 
 export const Hero = () => {
   return (
-    <section className="mt-10 flex flex-col xl:flex-row flex-nowrap justify-center gap-4 xl:gap-0">
-      <div className="basis-1/2 ps-20 flex flex-col items-center xl:items-end pe-20 bg-red-500">
-        <h1 className="text-7xl font-black text-center xl:text-start" style={{ width: "30rem" }}>
+    <section className="flex flex-col md:flex-row flex-nowrap justify-center mt-10">
+      <div className="basis-1/2 flex flex-col bg-red-500 p-2">
+        <h1 className="font-black text-center text-4xl">
           Transformez votre idée en réalité avec le no-code
         </h1>
-        <p className="text-xl pt-3 text-purple-100 text-center xl:text-start" style={{ width: "30rem" }}>
+        <p className="pt-3 text-purple-100 text-center">
           Créez votre site web, même si vous n'avez jamais codé auparavant.
         </p>
       </div>
-      <div className="basis-1/2 flex justify-center xl:justify-start bg-green-500">
-        <div style={{ width: "35rem" }}>
-          <CanvasAnimation/>
-          <div className="flex mt-3">
-            <Countdown />
-            <div className="flex flex-col ps-3 w-full justify-end">
-              <label className="mb-2">
-                Accédez à la <span className="font-bold">bêta</span> en
-                avant-première :
-              </label>
-              <div className="flex gap-1">
-                <input
-                  data-theme="light"
-                  type="text"
-                  placeholder="Entrez votre mail"
-                  className="input input-bordered"
-                />
-                <button data-theme="light" className="btn">M'inscrire</button>
-              </div>
+      <div className="basis-1/2 flex flex-col justify-center xl:justify-start bg-green-500 p-3">
+        <CanvasAnimation />
+        <div className="flex mt-3 flex-col-reverse sm:flex-row gap-3 sm:gap-0 items-center">
+          <Countdown />
+          <div className="flex flex-col w-full justify-end items-start sm:items-start">
+            <label className="mb-2">
+              Accédez à la <span className="font-bold">bêta</span> en
+              avant-première :
+            </label>
+            <div className="flex gap-1 w-full">
+              <input
+                data-theme="light"
+                type="text"
+                placeholder="Entrez votre mail"
+                className="input input-bordered w-full"
+              />
+              <button data-theme="light" className="btn">
+                Go
+              </button>
             </div>
           </div>
         </div>
